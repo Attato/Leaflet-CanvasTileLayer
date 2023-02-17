42a99f06a90c6dcfd345713c2253907c6b7b6225
KoffeMeow <guyden999@mail.ru>
2023-02-17 14:32:36 +0300

    marker init

--- components/map/map.tsx
+++ components/map/map.tsx
@@ -4,22 +4,33 @@ import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import {
+	MapContainer,
+	TileLayer,
+	ZoomControl,
+	Marker,
+	Popup,
+} from 'react-leaflet';
 
 import style from './map.module.scss';
 
 const Map = () => {
 	return (
 		<MapContainer
-			center={{ lat: 51.505, lng: -0.09 }}
-			zoom={13}
-			scrollWheelZoom={false}
+			center={{ lat: 60.009, lng: 30.377 }}
+			zoom={15}
+			zoomControl={false}
+			scrollWheelZoom={true}
 			className={style.map}
 		>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
+			<Marker position={{ lat: 60.00927, lng: 30.3772 }}>
+				<Popup>ООО "СТЦ"</Popup>
+			</Marker>
+			<ZoomControl position="bottomright" />
 		</MapContainer>
 	);
 };
