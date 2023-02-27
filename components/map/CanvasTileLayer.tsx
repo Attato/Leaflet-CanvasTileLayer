7420bc4f9bb3794d3f2c4c7d745f2c34b8428788
KoffeMeow <guyden999@mail.ru>
2023-02-27 19:06:49 +0300

    improved typing

--- components/map/CanvasTileLayer.tsx
+++ components/map/CanvasTileLayer.tsx
@@ -1,7 +1,7 @@
-import L from "leaflet";
+import L from 'leaflet';
 
 class CanvasTileLayer extends L.TileLayer {
-	tileSize: number | L.Point;
+	tileSize: number | L.Point | undefined;
 
 	constructor(urlTemplate: string, options?: L.TileLayerOptions) {
 		super(urlTemplate, options);
@@ -10,7 +10,7 @@ class CanvasTileLayer extends L.TileLayer {
 
 	onAdd(map: L.Map): this {
 		super.onAdd(map);
-		map.on("move", this.update, this);
+		map.on('move', this.update, this);
 		this.update();
 
 		return this;
@@ -18,70 +18,70 @@ class CanvasTileLayer extends L.TileLayer {
 
 	onRemove(map: L.Map): this {
 		super.onRemove(map);
-		map.off("move", this.update, this);
+		map.off('move', this.update, this);
 
 		return this;
 	}
 
 	createTile(coords: L.Coords, done: L.DoneCallback): HTMLCanvasElement {
-		const canvas = document.createElement("canvas");
-		const size = this.getTileSize();
+		const canvas: HTMLCanvasElement = document.createElement('canvas');
+		const size: L.Point = this.getTileSize();
 		canvas.width = size.x;
 		canvas.height = size.y;
 
-		const ctx = canvas.getContext("2d");
+		const ctx = canvas.getContext('2d');
 		if (!ctx) {
-			throw new Error("Could not create canvas context");
+			throw new Error('Could not create canvas context');
 		}
 
-		const img = new Image();
+		const img: HTMLImageElement = new Image();
 		img.onload = () => {
 			ctx.drawImage(img, 0, 0);
 			done(undefined, canvas);
 		};
 		img.onerror = (e) => {
-			done(new Error("error"), canvas);
+			done(new Error('error'), canvas);
 		};
 		img.src = this.getTileUrl(coords);
 		return canvas;
 	}
 
 	update() {
-		const map = this._map;
+		const map: L.Map = this._map;
 		if (!map) {
 			return;
 		}
 
-		var bounds = map.getPixelBounds();
-		var zoom = map.getZoom();
+		const bounds: L.Bounds = map.getPixelBounds();
+		const zoom: number = map.getZoom();
 
 		// get NorthWest and SouthEast points
-		var nwTilePoint = new L.Point(
+		const nwTilePoint = new L.Point(
 			// @ts-ignore
 			Math.floor(bounds.min.x / this.tileSize),
 			// @ts-ignore
-			Math.floor(bounds.min.y / this.tileSize),
+			Math.floor(bounds.min.y / this.tileSize)
 		);
 
-		var seTilePoint = new L.Point(
+		const seTilePoint = new L.Point(
 			// @ts-ignore
 			Math.floor(bounds.max.x / this.tileSize),
 			// @ts-ignore
-			Math.floor(bounds.max.y / this.tileSize),
+			Math.floor(bounds.max.y / this.tileSize)
 		);
 
 		// get max number of tiles in this zoom level
 
 		// @ts-ignore
-		var max = map.options.crs.scale(zoom) / this.tileSize;
+		const max: number = map.options.crs.scale(zoom) / this.tileSize;
 
 		// enumerate visible tiles
-		for (var x = nwTilePoint.x; x <= seTilePoint.x; x++) {
-			for (var y = nwTilePoint.y; y <= seTilePoint.y; y++) {
-				var xTile = (x + max) % max;
-				var yTile = (y + max) % max;
+		for (let x = nwTilePoint.x; x <= seTilePoint.x; x++) {
+			for (let y = nwTilePoint.y; y <= seTilePoint.y; y++) {
+				const xTile = (x + max) % max;
+				const yTile = (y + max) % max;
 
-				console.log("tile " + xTile + " " + yTile);
+				console.log('tile ' + xTile + ' ' + yTile);
 			}
 		}
 	}
