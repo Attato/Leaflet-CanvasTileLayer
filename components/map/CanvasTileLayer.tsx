d380f662bb91d5e37542474f286a2d2e60ce22a6
KoffeMeow <guyden999@mail.ru>
2023-02-27 12:40:13 +0300

    canvas update

--- components/map/CanvasTileLayer.tsx
+++ components/map/CanvasTileLayer.tsx
@@ -1,42 +1,117 @@
-import L from "leaflet";
+import L from 'leaflet';
 
-// Определяем размер тайла
-const tileSize = 256;
-
-// Определяем класс для нашего CanvasTileLayer
 class CanvasTileLayer extends L.TileLayer {
-	// Создаем тайл
-	createTile(coords: L.Coords, done: L.DoneCallback) {
-		// Создаем canvas элемент
-		const canvas = L.DomUtil.create(
-			"canvas",
-			"leaflet-tile",
-		) as HTMLCanvasElement;
+	canvas: HTMLCanvasElement;
+	ctx: CanvasRenderingContext2D | null;
+	tiles: { [key: string]: HTMLImageElement };
+	tileSize: number | L.Point;
+	currentView: L.Bounds;
 
-		// Задаем размер canvas
-		canvas.width = tileSize;
-		canvas.height = tileSize;
+	constructor(urlTemplate: string, options?: L.TileLayerOptions) {
+		super(urlTemplate, options);
 
-		// Создаем новый Image элемент
-		const img = new Image();
+		this.canvas = document.createElement('canvas');
+		this.ctx = this.canvas.getContext('2d');
+		this.tiles = {};
+		this.tileSize = options?.tileSize || 256;
+		this.currentView = new L.Bounds();
+	}
 
-		// Задаем обработчики событий для загрузки и ошибки
-		img.onload = () => {
-			const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-			ctx.drawImage(img, 0, 0, tileSize, tileSize);
+	onAdd(map: L.Map): this {
+		super.onAdd(map);
+		this.canvas.width = map.getSize().x;
+		this.canvas.height = map.getSize().y;
+		map.on('move', this.update, this);
+		this.update();
+
+		return this;
+	}
+
+	onRemove(map: L.Map): this {
+		super.onRemove(map);
+		map.off('move', this.update, this);
+
+		return this;
+	}
+
+	update() {
+		const map = this._map;
+		if (!map) {
+			return;
+		}
+
+		const pixelBounds = map.getPixelBounds();
+		const tileSize = this.tileSize;
+		const nwTilePoint = new L.Point(
 			// @ts-ignore
-			done(null, canvas);
+			Math.floor(pixelBounds.min.x / tileSize),
+			// @ts-ignore
+			Math.floor(pixelBounds.min.y / tileSize)
+		);
+
+		console.log(nwTilePoint);
+
+		const seTilePoint = new L.Point(
+			// @ts-ignore
+			Math.floor(pixelBounds.max.x / tileSize),
+			// @ts-ignore
+			Math.floor(pixelBounds.max.y / tileSize)
+		);
+
+		console.log(seTilePoint);
+
+		const tileBounds = new L.Bounds(nwTilePoint, seTilePoint);
+
+		if (this.currentView.equals(tileBounds)) {
+			return;
+		}
+
+		this.currentView = tileBounds;
+
+		console.log(this.currentView);
+
+		const tiles: Record<string, HTMLImageElement> = {};
+		for (let x = nwTilePoint.x; x <= seTilePoint.x; x++) {
+			for (let y = nwTilePoint.y; y <= seTilePoint.y; y++) {
+				const tileKey = `${x}:${y}`;
+				if (this._tiles[tileKey]) {
+					tiles[tileKey] = this._tiles[tileKey];
+					delete this._tiles[tileKey];
+				} else {
+					this.loadTile(x, y, tileKey);
+				}
+			}
+		}
+		for (const tileKey in this._tiles) {
+			this.removeTile(tileKey);
+		}
+		this._tiles = tiles;
+	}
+
+	loadTile(x: number, y: number, tileKey: string) {
+		const img = new Image();
+		img.crossOrigin = 'anonymous';
+		img.onload = () => {
+			this._tiles[tileKey] = img;
+			this.drawTile(x, y, img);
 		};
+		img.src = this.getTileUrl(x, y);
+	}
 
-		img.onerror = () => {
-			done(new Error(`Failed to load tile ${this.getTileUrl(coords)}`));
-		};
+	removeTile(tileKey: string) {
+		const img = this._tiles[tileKey];
+		if (img && img.parentNode) {
+			img.parentNode.removeChild(img);
+		}
+		delete this._tiles[tileKey];
+	}
 
-		// Устанавливаем путь к изображению
-		img.src = this.getTileUrl(coords);
-
-		// Возвращаем canvas элемент
-		return canvas;
+	drawTile(x: number, y: number, img: HTMLImageElement) {
+		const tileSize = this.tileSize;
+		const ctx = this.ctx;
+		const canvasX = x * tileSize;
+		const canvasY = y * tileSize;
+		ctx?.drawImage(img, canvasX, canvasY, tileSize, tileSize);
 	}
 }
 
--- components/map/map.tsx
+++ components/map/map.tsx
@@ -1,30 +1,33 @@
-"use client";
-import React, { useState, useEffect, useRef } from "react";
-import L from "leaflet";
-import CanvasTileLayer from "./CanvasTileLayer";
+'use client';
+import React, { useEffect } from 'react';
 
-function Map() {
-	const ref = useRef<any>();
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import CanvasTileLayer from './CanvasTileLayer';
+
+const Map = () => {
 	useEffect(() => {
-		if (ref.current) {
-			return;
-		}
-		ref.current = L.map("map", {
-			center: [-29.5, 145],
-			zoom: 3.5,
-		});
+		const map = L.map('map', { renderer: L.canvas() }).setView(
+			[60.00927, 30.3772],
+			17
+		);
 
 		const canvasTileLayer = new CanvasTileLayer(
-			"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+			'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 			{
-				attribution:
-					'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+				attribution: 'Map data &copy; OpenStreetMap contributors',
 				maxZoom: 18,
-			},
-		).addTo(ref.current);
+				tileSize: 256,
+			}
+		).addTo(map);
+
+		return () => {
+			map.remove();
+		};
 	}, []);
 
 	return <div id="map"></div>;
-}
+};
 
 export default Map;
