42a0147302a121f73564d521ba58b1573ab25f83
KoffeMeow <guyden999@mail.ru>
2023-03-02 10:29:31 +0300

    switch to canvas

--- components/map/CanvasTileLayer.tsx
+++ components/map/CanvasTileLayer.tsx
@@ -2,88 +2,48 @@ import L from 'leaflet';
 
 class CanvasTileLayer extends L.TileLayer {
 	tileSize: number | L.Point | undefined;
+	canvas: HTMLCanvasElement;
+	ctx: CanvasRenderingContext2D | null;
 
 	constructor(urlTemplate: string, options?: L.TileLayerOptions) {
 		super(urlTemplate, options);
 		this.tileSize = options?.tileSize || 256;
+		this.canvas = L.DomUtil.create('canvas', 'leaflet-tile-pane');
+		this.ctx = this.canvas.getContext('2d');
+	}
+
+	createTile(coords: L.Coords, done: L.DoneCallback): HTMLElement {
+		const tile = super.createTile(coords, done);
+		const url = this.getTileUrl(coords);
+
+		this.handleTileLoad(tile, url);
+		return tile;
+	}
+
+	private handleTileLoad(tile: HTMLElement, url: string) {
+		const img = new Image();
+		img.src = url;
+		console.log(url);
+
+		img.onload = () => {
+			this.removeTileElement(tile);
+
+			this.ctx
+				? this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height)
+				: console.log('!ctx');
+		};
+	}
+
+	removeTileElement(tile: HTMLElement) {
+		tile.parentNode?.removeChild(tile);
 	}
 
 	onAdd(map: L.Map): this {
 		super.onAdd(map);
-		map.on('move', this.update, this);
-		this.update();
+
+		this.getPane()?.appendChild(this.canvas);
 
 		return this;
-	}
-
-	onRemove(map: L.Map): this {
-		super.onRemove(map);
-		map.off('move', this.update, this);
-
-		return this;
-	}
-
-	createTile(coords: L.Coords, done: L.DoneCallback): HTMLCanvasElement {
-		const canvas: HTMLCanvasElement = document.createElement('canvas');
-		const size: L.Point = this.getTileSize();
-		canvas.width = size.x;
-		canvas.height = size.y;
-
-		const ctx = canvas.getContext('2d');
-		if (!ctx) {
-			throw new Error('Could not create canvas context');
-		}
-
-		const img: HTMLImageElement = new Image();
-		img.onload = () => {
-			ctx.drawImage(img, 0, 0);
-			done(undefined, canvas);
-		};
-		img.onerror = (e) => {
-			done(new Error('error'), canvas);
-		};
-		img.src = this.getTileUrl(coords);
-		return canvas;
-	}
-
-	update() {
-		const map: L.Map = this._map;
-		if (!map) {
-			return;
-		}
-
-		const bounds: L.Bounds = map.getPixelBounds();
-		const zoom: number = map.getZoom();
-
-		// get NorthWest and SouthEast points
-		const nwTilePoint = new L.Point(
-			// @ts-ignore
-			Math.floor(bounds.min.x / this.tileSize),
-			// @ts-ignore
-			Math.floor(bounds.min.y / this.tileSize)
-		);
-
-		const seTilePoint = new L.Point(
-			// @ts-ignore
-			Math.floor(bounds.max.x / this.tileSize),
-			// @ts-ignore
-			Math.floor(bounds.max.y / this.tileSize)
-		);
-
-		// get max number of tiles in this zoom level
-
-		// @ts-ignore
-		const max: number = map.options.crs.scale(zoom) / this.tileSize;
-
-		// enumerate visible tiles
-		for (let x = nwTilePoint.x; x <= seTilePoint.x; x++) {
-			for (let y = nwTilePoint.y; y <= seTilePoint.y; y++) {
-				const xTile = (x + max) % max;
-				const yTile = (y + max) % max;
-
-				console.log('tile ' + xTile + ' ' + yTile);
-			}
-		}
 	}
 }
 
--- components/map/map.tsx
+++ components/map/map.tsx
@@ -1,25 +1,25 @@
-"use client";
-import React, { useEffect } from "react";
+'use client';
+import React, { useEffect } from 'react';
 
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
-import CanvasTileLayer from "./CanvasTileLayer";
+import CanvasTileLayer from './CanvasTileLayer';
 
 const Map = () => {
 	useEffect(() => {
-		const map = L.map("map", { renderer: L.canvas() }).setView(
+		const map = L.map('map', { renderer: L.canvas() }).setView(
 			[60.00927, 30.3772],
-			17,
+			17
 		);
 
 		const canvasTileLayer = new CanvasTileLayer(
-			"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+			'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 			{
-				attribution: "Map data &copy; OpenStreetMap contributors",
+				attribution: 'Map data &copy; OpenStreetMap contributors',
 				maxZoom: 18,
 				tileSize: 256,
-			},
+			}
 		).addTo(map);
 
 		return () => {
