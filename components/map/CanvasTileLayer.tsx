4aa0b715cc5a7649c33712d32eb42cc3b381b825
KoffeMeow <guyden999@mail.ru>
2023-03-09 12:22:25 +0300

    trying a new method

--- components/map/CanvasTileLayer.tsx
+++ components/map/CanvasTileLayer.tsx
@@ -10,6 +10,7 @@ class CanvasTileLayer extends L.TileLayer {
 		this.tileSize = this.getTileSize();
 		this.canvas = L.DomUtil.create('canvas', 'leaflet-tile-pane');
 		this.ctx = this.canvas.getContext('2d');
+
 		this.canvas.width = window.innerWidth;
 		this.canvas.height = window.innerHeight;
 	}
@@ -18,13 +19,40 @@ class CanvasTileLayer extends L.TileLayer {
 		const tile = super.createTile(coords, done);
 		const url = this.getTileUrl(coords);
 
-		this.handleTileLoad(tile, url, coords);
+		this.canvasRedraw(tile, url, coords);
 		return tile;
 	}
 
-	private handleTileLoad(tile: HTMLElement, url: string, coords: L.Coords) {
+	private canvasRedraw(tile: HTMLElement, url: string, coords: L.Coords) {
 		const map: L.Map = this._map;
 		const bounds = map.getPixelBounds();
+
+		// @ts-ignore
+		const pos = this._getTilePos(coords);
+
+		tile.onload = () => {
+			// delete the original tile that was created with createTile
+			this.removeTileElement(tile);
+
+			this.ctx?.drawImage(
+				// @ts-ignore
+				tile,
+				// Need to find out the difference in coordinates after onmoveend and then subtract it from pos
+				pos.x,
+				pos.y,
+				this.tileSize.x,
+				this.tileSize.y
+			);
+
+			const imageData = this.ctx?.getImageData(
+				0,
+				0,
+				this.canvas.width,
+				this.canvas.height
+			);
+
+			console.log(imageData);
+		};
 
 		map.on('moveend', () => {
 			const newBounds = map.getPixelBounds();
@@ -34,23 +62,9 @@ class CanvasTileLayer extends L.TileLayer {
 			// @ts-ignore
 			const deltaY = Math.floor(newBounds.min.y - bounds.min.y);
 
-			console.log(deltaX, deltaY);
-			tile.onload = () => {
-				// удаляем оригинальный тайл, который был создан с помощью createTile
-				this.removeTileElement(tile);
-				// @ts-ignore
-				const pos = this._getTilePos(coords);
-
-				this.ctx?.drawImage(
-					// @ts-ignore
-					tile,
-					pos.x - deltaX,
-					pos.y - deltaY,
-					// Надо узнать разницу координат мувенда и потом вычитать её из pos
-					this.tileSize.x,
-					this.tileSize.y
-				);
-			};
+			pos.x -= deltaX;
+			pos.y -= deltaY;
+			L.DomUtil.setPosition(this.canvas, pos.subtract([deltaX, deltaY]));
 		});
 
 		tile.onerror = () => {
--- components/map/map.tsx
+++ components/map/map.tsx
@@ -1,26 +1,33 @@
-"use client";
-import React, { useEffect } from "react";
+'use client';
+import React, { useEffect } from 'react';
 
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
-import CanvasTileLayer from "./CanvasTileLayer";
+import CanvasTileLayer from './CanvasTileLayer';
 
 const Map = () => {
 	useEffect(() => {
-		const map = L.map("map", { renderer: L.canvas() }).setView(
-			[60.00927, 30.3772],
-			17,
-		);
+		const map = L.map('map').setView([60.00927, 30.3772], 17);
 
 		const canvasTileLayer = new CanvasTileLayer(
-			"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+			'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 			{
-				attribution: "Map data &copy; OpenStreetMap contributors",
+				attribution: 'Map data &copy; OpenStreetMap contributors',
 				maxZoom: 18,
 				tileSize: 256,
-			},
+			}
 		).addTo(map);
+
+		// const polyline = L.polyline(
+		// 	[
+		// 		[60.00927, 30.3772],
+		// 		[60.0063, 30.389],
+		// 		[60.008, 30.37],
+		// 		[60.00927, 30.3772],
+		// 	],
+		// 	{ color: 'red' }
+		// ).addTo(map);
 
 		return () => {
 			map.remove();
